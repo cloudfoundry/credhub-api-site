1f8a4c80fda50e7cb608467e6c0ce7d3713372a9
Marty Spiewak <mspiewak@pivotal.io>
2018-10-17 15:44:48 -0400

    fixing links to include version
    Signed-off-by: Chhavi Kankaria <ckankaria@pivotal.io>

--- config.rb
+++ config.rb
@@ -48,5 +48,5 @@ end
 # If you want Middleman to listen on a different port, you can set that below
 set :port, 4567
 
-config[:kitten] = File.read("versionfile")
+config[:version] = File.read("versionfile")
 
